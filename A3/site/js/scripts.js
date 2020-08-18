1deca9137fd9824a5346454f32ef369dc4a6f9a1
s2a <sivashambhu@gmail.com>
2020-08-18 15:35:42 +1000

    Site retry modifications

--- A3/site/js/scripts.js
+++ A3/site/js/scripts.js
@@ -78,50 +78,82 @@ function upload_to_lambda_a1(fieldName, file, metadata, load, error, progress, a
   var formData = new FormData();
   formData.append(file.name, file)
 
-  progress(true, 0.25, 1);
-  // Custom ajax file upload or local storing here
+  progress(false, 0, 1);
+
+  max_retry_count = 5
+  step_size = 1 / max_retry_count
+  step_count = 0
+  is_success = false
+
+  var query_data = (count, callback) => {
+  console.log('Query count ' + count.toString())
   $.ajax({
-    async: true,
-    crossDomain: true,
-    tryCount: 0,
-    retryLimit : 3,
-    method: 'POST',
-    url: 'https://c1ad8k6is0.execute-api.ap-south-1.amazonaws.com/dev/assignment2',
-    data: formData,
-    processData: false,
-    contentType: false,
-    mimeType: "multipart/form-data",
-    timeout: 5000,
-    fail: (xhr, textStatus, errorThrown) => error(errorThrown)
-  })
-  .done((response) => {
-    console.log(response)
+      async: true,
+      crossDomain: true,
+      tryCount: 0,
+      retryLimit : 3,
+      method: 'POST',
+      url: 'https://c1ad8k6is0.execute-api.ap-south-1.amazonaws.com/dev/assignment2',
+      data: formData,
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+      timeout: 5000
+    })
+    .fail((xhr, textStatus, errorThrown) => {
+      console.log('Retrying..')
+      if(count > 0){
+        status = 'retrying'
+        callback(status, null)
+        setTimeout(() => query_data(--count, callback), 5000)
+      } else {
+        status = 'fail'
+        callback(status, null)
+      }
+    })
+    .done((response) => {
+      status = 'success'
+      callback(status, response)
+    })
+  }
+
+  query_data(max_retry_count, (status, response) => {
+    console.log(status, response)
     $('#result-a1').css('visibility', 'visible');
-    $('#result-a1').attr('class', 'alert alert-success');
-    $('#result-a1-prediction').empty()
-    response = JSON.parse(response)
-    $('#result-a1-prediction').append('<div class="text-left">')
-      Object.keys(response).forEach((key) => {
-        $('#result-a1-prediction').append(
-          '<p>' 
-            + '<h5 class="text-uppercase">' + key + '</h5>' + response[key] 
-        + '</p>');
-    });
-    $('#result-a1-prediction').append('</div>')
-  })
-  .fail(() => {
-    $('#result-a2').css('visibility', 'visible')
-    $('#result-a2').attr('class', 'alert alert-danger');
-    $('#result-a2-prediction').text('Operation failed, please retry')
-  })
-  progress(true, 1, 1);
-  load('unique-file-id');
+    if(status == 'fail'){
+      ++step_count;
+      progress(true, 1, 1);
+      $('#result-a1').attr('class', 'alert alert-danger');
+      $('#result-a1-prediction').text('Operation failed, please retry')
+      error('Failed after ' + max_retry_count.toString() + 'retries');
+    } else if (status == 'retrying') {
+      progress(true, ++step_count * step_size, 1);
+      $('#result-a1').attr('class', 'alert alert-warning');
+      $('#result-a1-prediction').text('Please wait ...')
+    } else {
+      progress(true, 1, 1);
+      $('#result-a1').attr('class', 'alert alert-success');
+      $('#result-a1-prediction').empty()
+      response = JSON.parse(response)
+      $('#result-a1-prediction').append('<div class="text-left">')
+        Object.keys(response).forEach((key) => {
+          $('#result-a1-prediction').append(
+            '<p>' 
+              + '<h5 class="text-uppercase">' + key + '</h5>' + response[key] 
+          + '</p>');
+      });
+      $('#result-a1-prediction').append('</div>')
+    }
+  });
+
+  // load('unique-file-id');
   return {
     abort: function() {
       abort();
     }
   };
 }
+
 
 pond_a1.setOptions({
   allowReplace: false,
@@ -138,54 +170,83 @@ const inputElement2 = document.getElementById('filepond-a2');
 const pond_a2 = FilePond.create( inputElement2 );
 
 function upload_to_lambda_a2(fieldName, file, metadata, load, error, progress, abort) {
-  $('#result-a1').css('visibility', 'hidden');
+  $('#result-a2').css('visibility', 'hidden');
   var formData = new FormData();
   formData.append(file.name, file)
 
-  progress(true, 0.25, 1);
-  // Custom ajax file upload or local storing here
+  max_retry_count = 5
+  step_size = 1 / max_retry_count
+  step_count = 0
+  
+  progress(false, 0, 1);
+  
+  var query_data = (count, callback) => {
+  console.log('Query count ' + count.toString())
   $.ajax({
-    async: true,
-    crossDomain: true,
-    tryCount: 0,
-    retryLimit : 3,
-    method: 'POST',
-    url: 'https://ob38c41f22.execute-api.ap-south-1.amazonaws.com/dev/assignment2',
-    data: formData,
-    processData: false,
-    contentType: false,
-    mimeType: "multipart/form-data",
-    timeout: 5000,
-    fail: (xhr, textStatus, errorThrown) => error(errorThrown)
-  })
-  .done((response) => {
+      async: true,
+      crossDomain: true,
+      tryCount: 0,
+      retryLimit : 3,
+      method: 'POST',
+      url: 'https://ob38c41f22.execute-api.ap-south-1.amazonaws.com/dev/assignment2',
+      data: formData,
+      processData: false,
+      contentType: false,
+      mimeType: "multipart/form-data",
+      timeout: 5000
+    })
+    .fail((xhr, textStatus, errorThrown) => {
+      console.log('Retrying..')
+      if(count > 0){
+        status = 'retrying'
+        callback(status, null)
+        setTimeout(() => query_data(--count, callback), 5000)
+      } else {
+        status = 'fail'
+        callback(status, null)
+      }
+    })
+    .done((response) => {
+      status = 'success'
+      callback(status, response)
+    })
+  }
+
+  query_data(max_retry_count, (status, response) => {
+    console.log(status, response)
     $('#result-a2').css('visibility', 'visible');
-    $('#result-a2').attr('class', 'alert alert-success');
-    $('#result-a2-prediction').empty()
-    response = JSON.parse(response)
-    console.log(response)
+    if(status == 'fail'){
+      ++step_count;
+      progress(true, 1, 1);
+      $('#result-a2').attr('class', 'alert alert-danger');
+      $('#result-a2-prediction').text('Operation failed, please retry')
+      error('Failed after ' + max_retry_count.toString() + 'retries');
+    } else if (status == 'retrying') {
+      progress(true, ++step_count * step_size, 1);
+      $('#result-a2').attr('class', 'alert alert-warning');
+      $('#result-a2-prediction').text('Please wait ...')
+    } else {
+      progress(true, 1, 1);
+      $('#result-a2').attr('class', 'alert alert-success');
+      $('#result-a2-prediction').empty()
+      response = JSON.parse(response)
+      $('#result-a2-prediction').append(
+        '<p>' 
+          + '<h5 class="text-uppercase">' + 'file' + '</h5>' + response['file'] 
+      + '</p>');
+  
+      $('#result-a2-prediction').append('<div class="text-left">')
+        Object.keys(response['predicted']).forEach((key) => {
+          $('#result-a2-prediction').append(
+            '<p>' 
+              + '<h5 class="text-uppercase">' + key + '</h5>' + response['predicted'][key].toString()
+          + '</p>');
+      });
+      $('#result-a2-prediction').append('</div>')
+    }
+  });
 
-    $('#result-a2-prediction').append(
-      '<p>' 
-        + '<h5 class="text-uppercase">' + 'file' + '</h5>' + response['file'] 
-    + '</p>');
-
-    $('#result-a2-prediction').append('<div class="text-left">')
-      Object.keys(response['predicted']).forEach((key) => {
-        $('#result-a2-prediction').append(
-          '<p>' 
-            + '<h5 class="text-uppercase">' + key + '</h5>' + response['predicted'][key].toString()
-        + '</p>');
-    });
-    $('#result-a2-prediction').append('</div>')
-  })
-  .fail(() => {
-    $('#result-a2').css('visibility', 'visible')
-    $('#result-a2').attr('class', 'alert alert-danger');
-    $('#result-a2-prediction').text('Operation failed, please retry')
-  })
-  progress(true, 1, 1);
-  load('unique-file-id');
+  // load('unique-file-id');
   return {
     abort: function() {
       abort();
@@ -213,34 +274,64 @@ function upload_to_lambda_a3(fieldName, file, metadata, load, error, progress, a
   var formData = new FormData();
   formData.append(file.name, file)
 
-  progress(true, 0.25, 1);
+  progress(false, 0, 1);
 
-  axios.post('https://2u5go6le4m.execute-api.ap-south-1.amazonaws.com/dev/assignment3-face-align', 
-             formData,
-             {responseType: 'blob'})
-  .then(function (response) {
-    console.log(response)
-    //var arrayBufferView = new Uint8Array( response['data'] );
-    //var blob = new Blob( [ arrayBufferView ], { type: "image/png" } );
-    var URL = window.URL || window.webkitURL;
-    blob = response['data']
-    var imageUrl = URL.createObjectURL( blob );
-    console.log(imageUrl)
-    
-    var img = new Image(100, 100);
-    // So the Blob can be Garbage Collected
-    img.onload = e => URL.revokeObjectURL( imageUrl );
-    img.src = imageUrl;
+  max_retry_count = 5
+  step_size = 1 / max_retry_count
+  step_count = 0
 
-    $('#result-a3').css('visibility', 'visible')
-    document.querySelector("#result-a3-prediction-img").src = imageUrl;
-  })
-  .catch(function (error) {
-    console.log(error);
+  var query_data = (count, callback) => {
+    console.log('Query count ' + count.toString())
+    axios.post('https://2u5go6le4m.execute-api.ap-south-1.amazonaws.com/dev/assignment3-face-align', 
+    formData,
+    {
+      responseType: 'blob',
+      timeout: 5000
+     }).then((response) => {
+      console.log(response)
+      status = 'success'
+      callback(status, response)
+      })
+      .catch((error) => {
+        if(count > 0){
+          status = 'retrying'
+          callback(status, null)
+          setTimeout(() => query_data(--count, callback), 5000)
+        } else {
+          status = 'fail'
+          callback(status, error)
+        }
+      });
+    }
+
+  query_data(max_retry_count, (status, response) => {
+    console.log(status, response)
+    $('#result-a3').css('visibility', 'visible');
+    if(status == 'fail'){
+      ++step_count;
+      progress(true, 1, 1);
+      $('#result-a3').attr('class', 'alert alert-danger');
+      $('#result-a3-prediction').text('Operation failed, please retry')
+      error('Failed after ' + max_retry_count.toString() + 'retries');
+    } else if (status == 'retrying') {
+      progress(true, ++step_count * step_size, 1);
+      $('#result-a3').attr('class', 'alert alert-warning');
+      $('#result-a3-prediction').text('Please wait ...')
+    } else {
+      $('#result-a3').attr('class', 'alert alert-success');
+      $('#result-a3-prediction').empty()
+      progress(true, 1, 1);
+      var URL = window.URL || window.webkitURL;
+      blob = response['data']
+      var imageUrl = URL.createObjectURL( blob );
+      var img = new Image(100, 100);
+      img.onload = e => URL.revokeObjectURL( imageUrl );
+      img.src = imageUrl;
+      $('#result-a3').css('visibility', 'visible')
+      document.querySelector("#result-a3-prediction-img").src = imageUrl;
+    }
   });
-
-  progress(true, 1, 1);
-  load('unique-file-id');
+  // load('unique-file-id');
   return {
     abort: function() {
       abort();
